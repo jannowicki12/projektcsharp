d00a93899b83a7ace453af5c10d2331dcae3152c
jannowicki12 <95223624+jannowicki12@users.noreply.github.com>
2023-02-08 14:02:50 +0100

    giga szef

---
+++ aplikacja/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using aplikacja.Data;
+using aplikacja.Models;
+using aplikacja.Models.Domain;
+using Azure.Core.GeoJson;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace aplikacja.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly CrudDbContext crudDbContext;
+
+        public EmployeesController(CrudDbContext crudDbContext)
+        {
+            this.crudDbContext = crudDbContext;
+        }
+        [HttpGet]
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Add(AddEmployeeViewModel add)
+        {
+            var empoyee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = add.Name,
+                Email = add.Email,
+                Salary = add.Salary,
+                Position = add.Position,
+                BirthDate = add.BirthDate,
+            };
+
+            await crudDbContext.Employees.AddAsync(empoyee);
+            await crudDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ aplikacja/Models/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace aplikacja.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Position { get; set; }
+    }
+}
